7ed1ce395046af82ddb56cec478070e6d26bdb7b
Gokul Krishna E <gokul44krishna@gmail.com>
2023-11-29 17:46:33 +0530

    natilea initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+// include html
+  let includes = $('[data-include]')
+  jQuery.each(includes, function(){
+    let html = '/' + $(this).data('include') + '.html'
+    $(this).load(html)
+  })
+
+})
+
+$(document).ajaxStop(function(){
+  $('.match-height').matchHeight()
+})
